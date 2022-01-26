76beed8242b1791adf1959340cf88fd907e04274
PePacheco <pedro_pacheco98@outlook.com>
2022-01-26 13:19:01 -0300

    Finished the episode details view

--- Jobsity-Test/Flows/Episode Details/EpisodeDetailsViewController.swift
+++ Jobsity-Test/Flows/Episode Details/EpisodeDetailsViewController.swift
@@ -8,18 +8,37 @@
 import UIKit
 
 class EpisodeDetailsViewController: UIViewController, Coordinating {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var episodeNumberLabel: UILabel!
+    @IBOutlet weak var seasonLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
     var coordinator: Coordinator?
     private var episode: Episode?
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let episode = episode else { return }
+        title = episode.name
+        configureUI()
     }
     
     // MARK: - Functions
     func setEpisode(episode: Episode) {
         self.episode = episode
+    }
+    
+    private func configureUI() {
+        guard let episode = episode else {
+            return
+        }
+        posterImageView.kf.setImage(with: episode.mediumImage)
+        episodeNumberLabel.text = "Episode \(episode.number)"
+        seasonLabel.text = "Season \(episode.season)"
+        summaryLabel.text = episode.summary.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
     }
 
 }
